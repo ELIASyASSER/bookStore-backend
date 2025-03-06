7b5a01bfb03399db1f64e0ef4202d9fa2881bbe4
elias <eliasyasser193@gmail.com>
2025-03-06 14:45:05 +0200

    f

--- db/books.js
+++ db/books.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connection = (url)=>{
-    return mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true })
+    return mongoose.connect(url)
 }
 
 export default connection
